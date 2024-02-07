b0829e1f6dbddbf3ea117a8450560bb51fc1f16e
BENwada <domo0103wada@gmail.com>
2024-02-07 16:36:29 +0900

    empty swicher add

---
+++ app/(dashboard)/_components/board-list.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { EmptyFavorites } from "./empty-favorites";
+import { EmptySearch } from "./empty-search";
+
+interface BoardListProps {
+  orgId: string;
+  query: {
+    search?: string;
+    favorites?: string;
+  };
+}
+
+export const BoardList = ({ orgId, query }: BoardListProps) => {
+  const data = []; // TODO: Change to API call
+
+  if (!data?.length && query.search) {
+    return <EmptySearch />;
+  }
+
+  if (!data?.length && query.favorites) {
+    return <EmptyFavorites />;
+  }
+
+  if (!data?.length) {
+    return <div>No boards at all</div>;
+  }
+
+  return <div className="">{JSON.stringify(query)}</div>;
+};
--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button";
 
 import Image from "next/image";
 import { cn } from "@/lib/utils";
+import { useSearchParams } from "next/navigation";
 
 const font = Poppins({
   subsets: ["latin"],
@@ -15,6 +16,9 @@ const font = Poppins({
 });
 
 const OrgSidebar = () => {
+  const searchParams = useSearchParams();
+  const favorites = searchParams.get("favorites");
+
   return (
     <div className="hidden lg:flex flex-col space-y-6 w-[206px] pl-5 pt-5">
       <Link href="/">
@@ -47,7 +51,7 @@ const OrgSidebar = () => {
       />
       <div className="space-y-1 w-full">
         <Button
-          variant="ghost"
+          variant={favorites ? "ghost" : "secondary"}
           asChild
           size="lg"
           className="font-normal justify-start px-2 w-full"
@@ -58,7 +62,7 @@ const OrgSidebar = () => {
           </Link>
         </Button>
         <Button
-          variant="ghost"
+          variant={favorites ? "secondary" : "ghost"}
           asChild
           size="lg"
           className="font-normal justify-start px-2 w-full"
