1425ffccfed5fee0d66c10c14235835877be56b2
BENwada <domo0103wada@gmail.com>
2024-02-13 18:56:05 +0900

    rename modal add

---
+++ providers/modal-provider.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { RenameModal } from "@/components/modals/rename-modal";
+
+export const ModalProvider = () => {
+  return (
+    <>
+      <RenameModal />
+    </>
+  );
+};
