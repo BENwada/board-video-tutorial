13e72225e84d53ee26ad959ba421ba1be9730746
BENwada <domo0103wada@gmail.com>
2024-02-17 17:58:45 +0900

    liveblocks add

--- liveblocks.config.ts
+++ liveblocks.config.ts
@@ -2,8 +2,7 @@ import { createClient } from "@liveblocks/client";
 import { createRoomContext } from "@liveblocks/react";
 
 const client = createClient({
-  publicApiKey:
-    "pk_dev_HSwhyaoko7Y8sCOiqL5gKU7S4jY07JZIRiqSlnAHUXxN_JFUB0AIvspHyfUSpPqz",
+  authEndpoint: "/api/liveblocks-auth",
 });
 
 // Presence represents the properties that exist on every user in the Room
